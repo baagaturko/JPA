5239f66c292968eec8e7a10aeed4f41d8cd49669
Khan <some@mail>
2017-08-18 12:58:29 +0300

    first commit

---
+++ src/main/java/golden/horde/JpAtkoApplication.java
@@ -0,0 +1,12 @@
+package golden.horde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAtkoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpAtkoApplication.class, args);
+	}
+}
